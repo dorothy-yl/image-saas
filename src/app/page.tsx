283ebdd6e74eb3a4d5b2baa085799d2dbed9f68a
dorothy <dorothy.luluya@gmail.com>
2025-10-02 15:35:07 +0800

    创建第一个页面

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,19 @@
 import { Button } from "@/components/Button";
+import { Input } from "@/components/Input";
 import { Textarea } from "@/components/Textarea";
-import { Input } from "@/components/input";
 
 export default function Home() {
-  return (
-   <div>
-    <Input name="name" />
-    <Textarea name="description" />
-    <Button type="submit">Submit</Button>
-   </div>
-  );
+    return (
+        <div className="h-screen flex justify-center items-center">
+            <form className="w-full max-w-md flex flex-col gap-4">
+                <h1 className="text-center text-2xl font-bold">Create App</h1>
+                <Input name="name" placeholder="App Name"></Input>
+                <Textarea
+                    name="description"
+                    placeholder="Description"
+                ></Textarea>
+                <Button type="submit">Submit</Button>
+            </form>
+        </div>
+    );
 }
